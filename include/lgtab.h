3b09d840898ee2c81aa243f8dc2faad61bcb4e9b
Leandre <leandre.blanchard@epitech.eu>
2018-09-03 19:27:45 +0200

    Clear function

--- include/lgtab.h
+++ include/lgtab.h
@@ -24,17 +24,21 @@ int gtab_create(gtab_t *gtab, size_t nsize);
 void gtab_destroy(gtab_t *gtab, void (* ifct)(void *));
 
 int gtab_resize(gtab_t *gtab, size_t newsize, void (* destructor)(void *));
+void gtab_clear(gtab_t *gtab, void (* destructor)(void *));
+
 int gtab_append(gtab_t *gtab, const void *add);
 int gtab_append_at(gtab_t *gtab, const void *add, size_t index);
+
 void gtab_remove(gtab_t *gtab, void *remove, void (* destructor)(void *));
 void gtab_remove_at(gtab_t *gtab, size_t index, void (* destructor)(void *));
+
 int gtab_shrink_to_fit(gtab_t *gtab);
 void gtab_swap(gtab_t *gtab, size_t p1, size_t p2);
 int gtab_copy(const gtab_t *src, gtab_t *dest, void *(*copier)(void *),
 		const void *errval);
+
 int gtab_sappend(gtab_t *gtab, const void *add);
 int gtab_sappend_at(gtab_t *gtab, const void *add, size_t pos);
-int gtab_clear(gtab_t *gtab, void (* destructor)(void *));
 
 int gtab_lock(gtab_t *gtab);
 int gtab_unlock(gtab_t *gtab);
